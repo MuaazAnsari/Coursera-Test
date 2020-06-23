c6ba74e37971584e9acaa3f2589c5c60807a6cc8
MuaazAnsari <65887127+MuaazAnsari@users.noreply.github.com>
2020-06-23 20:24:23 +0530

    Change

--- Module4Solution/SpeakGoodbye.js
+++ Module4Solution/SpeakGoodbye.js
@@ -1,9 +1,3 @@
-(function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
-
-})(window);
+const byeSpeaker = (name) {
+  console.log("GoodBye ",name);
+}
